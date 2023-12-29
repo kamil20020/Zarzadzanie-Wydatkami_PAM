9de4315f70e3d8e7da590bd5599259094fcf0e0d
kdywan_254049 <idywan_254049@e-science.pl>
2023-12-29 09:08:58 +0100

    First commit

---
+++ app/src/main/java/pwr/lab/expenses_management/MainActivity.java
@@ -0,0 +1,14 @@
+package pwr.lab.expenses_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
